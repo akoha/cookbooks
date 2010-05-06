e7c1669033d0adc43d866ae41847813bad7bd268
Erik Wright <erik@akoha.com>
2010-05-06 16:35:16 -0400

    Update the Jira cookbook to support Jira 4, SSL termination at Apache, and parameterized database name.

--- jira/recipes/default.rb
+++ jira/recipes/default.rb
@@ -34,6 +34,13 @@ include_recipe "apache2::mod_proxy"
 include_recipe "apache2::mod_proxy_http"
 include_recipe "apache2::mod_ssl"
 
+service "jira" do
+  supports :restart => true, :status => true
+  action :nothing
+end
+
+jira_major_version = node[:jira][:version].match(/^.*-(\d+)(\.\d+)*$/)[1].to_i  
+
 unless FileTest.exists?(node[:jira][:install_path])
   remote_file "jira" do
     path "/tmp/jira.tar.gz"
@@ -47,8 +54,7 @@ unless FileTest.exists?(node[:jira][:install_path])
   bash "install-jira" do
     code "mv /tmp/atlassian-jira-#{node[:jira][:version]}-standalone #{node[:jira][:install_path]}"
   end
-  
-  if node[:jira][:database] == "mysql"
+  if node[:jira][:database] == "mysql" and jira_major_version < 4
     remote_file "mysql-connector" do
       path "/tmp/mysql-connector.tar.gz"
       source "http://downloads.mysql.com/archives/mysql-connector-java-5.1/mysql-connector-java-5.1.6.tar.gz"
@@ -64,29 +70,43 @@ unless FileTest.exists?(node[:jira][:install_path])
   end
 end
 
-directory "#{node[:jira][:install_path]}" do
-  recursive true
-  owner "www-data"
+subdirs = ['', '/conf', '/logs', '/work']
+if jira_major_version >= 4
+  subdirs.push(*['/data', '/export', '/log', '/plugins', '/caches', '/tmp', '/temp'])
+end
+
+subdirs.each do |dir|
+  directory "#{node[:jira][:install_path]}#{dir}" do
+    owner node[:jira][:run_user]
+  end
 end
 
 remote_file "#{node[:jira][:install_path]}/bin/startup.sh" do
   source "startup.sh"
   mode 0755
+  notifies :restart, resources(:service => "jira")
 end
   
 remote_file "#{node[:jira][:install_path]}/bin/catalina.sh" do
   source "catalina.sh"
   mode 0755
+  notifies :restart, resources(:service => "jira")
 end
 
 template "#{node[:jira][:install_path]}/conf/server.xml" do
-  source "server.xml.erb"
+  if jira_major_version < 4
+    source "server.xml.erb"
+  else
+    source "server.xml.4.x.x.erb"
+  end
   mode 0755
+  notifies :restart, resources(:service => "jira")
 end
   
 template "#{node[:jira][:install_path]}/atlassian-jira/WEB-INF/classes/entityengine.xml" do
   source "entityengine.xml.erb"
   mode 0755
+  notifies :restart, resources(:service => "jira")
 end
 
 runit_service "jira"
@@ -94,6 +114,40 @@ runit_service "jira"
 template "#{node[:apache][:dir]}/sites-available/jira.conf" do
   source "apache.conf.erb"
   mode 0644
+  variables({
+    :port => 80
+  })
+  notifies :restart, resources(:service => "apache2")
 end
 
-apache_site "jira.conf"
+template "#{node[:apache][:dir]}/sites-available/jira-require-ssl.conf" do
+  source "apache.require-ssl-conf.erb"
+  mode 0644
+  variables({
+    :port => 80
+  })
+  notifies :restart, resources(:service => "apache2")
+end
+
+template "#{node[:apache][:dir]}/sites-available/jira-ssl.conf" do
+  source "apache.conf.erb"
+  mode 0644
+  variables({
+    :port => 443,
+    :ssl_certificate_path => node[:jira][:ssl_certificate_path],
+    :ssl_key_path => node[:jira][:ssl_key_path]
+  })
+  notifies :restart, resources(:service => "apache2")
+end
+
+apache_site "jira.conf" do
+  enable !node[:jira][:require_ssl]
+end
+
+apache_site "jira-require-ssl.conf" do
+  enable node[:jira][:require_ssl]
+end
+
+apache_site "jira-ssl.conf" do
+  enable node[:jira][:enable_ssl]
+end
